991ab8187f6279df767162b4ed56ff1a3ba888d5
cdufour <opusidea@gmail.com>
2018-07-16 17:16:49 +0200

    J1

---
+++ intro/src/Controller/DemoController.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Request;
+
+class DemoController extends Controller
+{
+  public $players = [
+    array(
+      'id' => 1,
+      'name' => 'Pogba',
+      'num' => 6,
+      'substitute' => false,
+      'photo' => 'http://cdn.sports.fr//images/media/football/coupe-du-monde-2018/equipe-de-france/articles/pogba-le-patron-a-parle/pogba/25738259-1-fre-FR/Pogba_w484.jpg'
+    ),
+    array(
+      'id' => 2,
+      'name' => 'Lloris',
+      'num' => 1,
+      'substitute' => true,
+      'photo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Lloris_2018_%28cropped%29.jpg/260px-Lloris_2018_%28cropped%29.jpg'
+    ),
+    array(
+      'id' => 3,
+      'name' => 'Mbappé',
+      'num' => 10,
+      'substitute' => false,
+      'photo' => 'https://le10static.com/img/a/2/6/5/6/2/4/265624-large.jpg'
+    )
+  ];
+
+  public function index()
+  {
+    $colors = ['bleu', 'blanc', 'rouge'];
+
+    // la méthode render() provient de la classe Controller
+    return $this->render('demo.html.twig', array(
+      'title' => 'Demo TWIG',
+      'message' => 'Simple message en provenance du controller',
+      'available' => true,
+      'colors' => $colors,
+      'players' => $this->players
+    ));
+  }
+
+  public function player($id)
+  {
+    // $id correspond au paramètre d'url {id} défini
+    // dans le fichier de routage
+
+    // à partir de l'identifiant, on va récupérer la
+    // totalité des données concernant le joueur identifié
+
+    $player = null;
+    foreach($this->players as $p) {
+      // joueur trouvé dans la source de données
+      if ($p['id'] == $id) {
+        $player = $p;
+      }
+    }
+
+    return $this->render('player.html.twig', array(
+      'player' => $player
+    ));
+  }
+}
